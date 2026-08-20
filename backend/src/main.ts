c090a58d5ee6799d7f8ff95f820c1f8114459ecc
Oruebor Manasseh <97970575+manaboy-07@users.noreply.github.com>
2026-08-21 00:32:28 +0100

    Updated cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,13 +6,12 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-   app.use(cookieParser());
+  app.use(cookieParser());
   app.setGlobalPrefix('Bloodline');
   app.enableCors({
-    origin: true,
+    origin: ['https://bloodline-frontend.vercel.app', 'http://localhost:4000'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
-
